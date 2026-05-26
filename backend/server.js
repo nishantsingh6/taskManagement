24a66fb5870c9886bf8bfcdb5e10647053718339
nishantsingh6 <nishantsingh2n@gmail.com>
2026-05-26 10:49:47 +0530

    Update frontend live url in backend + add vercel json file

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 connectDB();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://taskmanagement-szbq.onrender.com', credentials: true }));
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
